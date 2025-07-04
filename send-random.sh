e2a1c4791c1530fad88418774ff8b5d4805bbc61
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2025-07-04 12:37:01 +0700

    Update send-random.sh

--- send-random.sh
+++ send-random.sh
@@ -4,13 +4,13 @@
 wallet=$(shuf -n 1 send.txt)
 
 # Lấy ngẫu nhiên một dòng từ send-addr.txt (địa chỉ nhận)
-send_addr=$(shuf -n 1 send-addr.txt)
+addr=$(shuf -n 1 addr.txt)
 
 # Sinh số ngẫu nhiên từ 0.001 đến 0.009 với 3 chữ số thập phân
-random_vol=$(awk -v min=0.001 -v max=0.009 'BEGIN{srand(); printf "%.3f", min + rand() * (max - min)}')
+vol=$(awk -v min=0.001 -v max=0.009 'BEGIN{srand(); printf "%.3f", min + rand() * (max - min)}')
 
 # In lệnh để kiểm tra (bạn có thể bỏ dòng echo này nếu không cần)
-echo "python3 $wallet $send_addr $random_vol"
+echo "python3 $wallet $addr $vol"
 
 # Thực thi lệnh
-python3 "$wallet" "$send_addr" "$random_vol"
+python3 "$wallet" "$addr" "$vol"
