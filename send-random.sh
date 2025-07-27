d44fe2461189795c49768fdafb5d3924e70f22ed
hiepntnaa <160826386+hiepntnaa@users.noreply.github.com>
2025-07-27 13:00:27 +0700

    Update send-random.sh

--- send-random.sh
+++ send-random.sh
@@ -10,8 +10,8 @@ while true; do
     # Thực thi lệnh
     python3 "$wallet"
 
-    # Sinh thời gian ngủ ngẫu nhiên từ 10800 (180 phút) đến 12600 (210 phút) giây
-    sleep_time=$((RANDOM % 1801 + 10800)) 
+    # Sinh thời gian ngủ ngẫu nhiên từ 14400 (240 phút) đến 18000 (300 phút) giây
+    sleep_time=$((RANDOM % 3601 + 14400)) 
     echo "[$(date)] Sleeping for $sleep_time seconds (~$((sleep_time / 60)) minutes)..."
     sleep "$sleep_time"
 done
